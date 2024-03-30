28a6bf2230a574b0aed46e5c1f41764d8ce5763e
Oleg Kalachev <okalachev@gmail.com>
2024-03-30 18:23:42 +0300

    Add info about motors testing commands to intro message

--- flix/cli.ino
+++ flix/cli.ino
@@ -30,6 +30,7 @@ const char* motd =
 "cr - calibrate RC\n"
 "cg - calibrate gyro\n"
 "ca - calibrate accel\n"
+"mfr, mfl, mrr, mrl - test appropriate motor\n"
 "fullmot <n> - full motor test\n"
 "reset - reset drone's state\n";
 
