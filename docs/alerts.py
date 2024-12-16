135eae5a1cb551871494c7e637f7a79df420f6dc
Oleg Kalachev <okalachev@gmail.com>
2024-12-16 11:34:14 +0300

    Cleanup alerts script

--- docs/alerts.py
+++ docs/alerts.py
@@ -9,21 +9,18 @@ import re
 def transform_markdown_to_html(markdown_text):
     def replace_blockquote(match):
         tag = match.group(1).lower()
-        content = match.group(2).strip().replace("\n> ", " ")
+        content = match.group(2).strip().replace('\n> ', ' ')
         return f'<div class="alert alert-{tag}">{content}</div>\n'
 
     pattern = re.compile(r'> \[!(NOTE|TIP|IMPORTANT|WARNING|CAUTION)\]\n>(.*?)\n?(?=(\n[^>]|\Z))', re.DOTALL)
-
     transformed_text = pattern.sub(replace_blockquote, markdown_text)
-
     return transformed_text
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1: # we check if we received any argument
-        if sys.argv[1] == "supports":
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'supports':
             sys.exit(0)
-
 
     context, book = json.load(sys.stdin)
 
