8484854576493fd8300f15c2510aee34e6d51ef4
Oleg Kalachev <okalachev@gmail.com>
2025-11-01 23:55:55 +0300

    Keep only one floating point version of map function Two variants are redundant

--- flix/util.h
+++ flix/util.h
@@ -12,11 +12,7 @@
 const float ONE_G = 9.80665;
 extern float t;
 
-float mapf(long x, long in_min, long in_max, float out_min, float out_max) {
-	return (float)(x - in_min) * (out_max - out_min) / (float)(in_max - in_min) + out_min;
-}
-
-float mapff(float x, float in_min, float in_max, float out_min, float out_max) {
+float mapf(float x, float in_min, float in_max, float out_min, float out_max) {
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
